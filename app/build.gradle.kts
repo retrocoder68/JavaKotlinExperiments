78995f3255b786dfc5bc8e4950a59d9e87acf11b
skywalker <j.karlsson@retrocoder.se>
2024-05-26 09:49:23 +0200

    feat(JVKT-004): Added support for kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,9 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+
+    // Kotlin support
+    kotlin("jvm") version "2.0.0"
 }
 
 repositories {
